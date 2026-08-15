a4cba32df0106d02e8e609a0a98c5e4ad7024993
ki <kre1238@gmail.com>
2026-08-15 15:52:41 +0300

    add commander and started program

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/d8m8tra/backend-project-4/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/d8m8tra/backend-project-4/actions)
+
+[![GitHub Actions](https://github.com/d8m8tra/backend-project-4/actions/workflows/gh.yml/badge.svg)](https://github.com/d8m8tra/backend-project-4/actions/workflows/gh.yml)
--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
+// Реализуйте утилиту командной строки page-loader,сдлуфк
+// которая скачивает страницу из сети и кладёт в указанную директорию
+// (по умолчанию в директорию запуска программы).
+// Программа должна возвращать полный путь к загруженному файлу.
 
-const parser = () => {
-  console.log('test');
+import { program } from "commander";
+
+// page-loader --output /var/tmp https://ru.hexlet.io/courses
+
+const parser = (page) => {
+  console.log(page);
 };
 
 program
-  .version('0.0.1')
-  .action(parser)
-  .parse(process.argv)
-  
+  .name("page-loader")
+  .version("0.0.1")
+  .option("-o, --output [dir]", "output dir", "/home/user/current-dir")
+  .action(parser) // Когда программа будет запущена без какой-то специальной команды, выполни функцию
+  .arguments("<page>")
+  .parse(process.argv);
